c88d5f0e791d25330ef856204b3ae0f8a9b75a8a
Alexander Shangin <5kr1p7@gmail.com>
2018-11-29 13:37:33 +0300

    Add favicons
    Add generated favicons from realfavicongenerator.net

---
+++ resources/views/partials/meta.blade.php
@@ -0,0 +1,24 @@
+<!-- Meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="keywords" content="Шангин, 5kr1p7, Shangin, Александр Шангин" />
+    <meta name="description" content="Персональный сайт Александра Шангина" />
+    <meta name="author" content="Alexander M. Shangin" />
+    <meta http-equiv="content-language" content="{{ app()->getLocale() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=yyLoL2GPw3">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=yyLoL2GPw3">
+    <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png?v=yyLoL2GPw3">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=yyLoL2GPw3">
+    <link rel="manifest" href="/site.webmanifest?v=yyLoL2GPw3">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=yyLoL2GPw3" color="#5bbad5">
+    <link rel="shortcut icon" href="/favicon.ico?v=yyLoL2GPw3">
+    <meta name="apple-mobile-web-app-title" content="AlexShangin.Ru">
+    <meta name="application-name" content="AlexShangin.Ru">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=yyLoL2GPw3">
+
