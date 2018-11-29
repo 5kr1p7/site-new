3dfa6d8d1abd0950d6dc5b1da3ed7c6a1c97b817
Alexander Shangin <5kr1p7@gmail.com>
2018-11-29 13:38:49 +0300

    OpenGraph

---
+++ resources/views/partials/og.blade.php
@@ -0,0 +1,13 @@
+<!-- OpenGraph -->
+    <meta property="og:image:width" content="279">
+    <meta property="og:image:height" content="279">
+    <meta property="og:url" content="{{ URL::to('/') }}">
+    <meta property="og:image" content="{{ URL::to('/') }}/og-image.png">
+    <meta property="og:title" content="AlexShangin.Ru">
+    <meta property="og:description" content="Персональный сайт Александра Шангина">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@5kr1p7">
+    <meta name="twitter:title" content="AlexShangin.Ru">
+    <meta name="twitter:description" content="Персональный сайт Александра Шангина.">
+
