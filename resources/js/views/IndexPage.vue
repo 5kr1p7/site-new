ea21944198f2f38fe729794945d91cf59445d55a
Alexander Shangin <5kr1p7@gmail.com>
2018-11-29 15:14:19 +0300

    Index page view

---
+++ resources/js/views/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-layout align-center justify-center>
+        <p class="text-xs-center display-3">Сайт находится в разработке.</p>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "IndexPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
