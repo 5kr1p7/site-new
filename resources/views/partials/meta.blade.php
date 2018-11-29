0f1cf35cdef1543dce365b499218469eee6ba237
Alexander Shangin <5kr1p7@gmail.com>
2018-11-29 15:13:23 +0300

    Update viewport

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -2,7 +2,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     <meta name="keywords" content="Шангин, 5kr1p7, Shangin, Александр Шангин" />
     <meta name="description" content="Персональный сайт Александра Шангина" />
     <meta name="author" content="Alexander M. Shangin" />
