685f2da1bbf945fbbffeddd8e975a2b871568da6
Alexander Shangin <5kr1p7@gmail.com>
2018-11-29 15:14:59 +0300

    Set page structure for Vuetify

--- resources/views/partials/skeleton.blade.php
+++ resources/views/partials/skeleton.blade.php
@@ -5,14 +5,27 @@
     @include('partials.og')
 
     <!-- Styles -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
+    <link href="//unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <title>@yield('title') — AlexShangin.Ru</title>
 </head>
 <body>
     <div id="app">
-        @yield('content')
+        <v-app light>
+            <div>
+                <v-content class="flex-center position-ref full-height">
+                    <v-container fluid>
+                        <div class="content">
+                            <router-view></router-view>
+                        </div>
+                    </v-container>
+                </v-content>
+            </div>
+        </v-app>
     </div>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="//cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
 </body>
 </html>
