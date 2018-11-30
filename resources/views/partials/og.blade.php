53569ef81600f8baf5630eab9434cfa113bb63f8
Alexander Shangin <5kr1p7@gmail.com>
2018-11-30 13:51:57 +0300

    Change OpenGraph image size

--- resources/views/partials/og.blade.php
+++ resources/views/partials/og.blade.php
@@ -2,8 +2,8 @@
     <meta property="og:site_name" content="AlexShangin.Ru">
     <meta property="og:title" content="AlexShangin.Ru">
     <meta property="og:description" content="Персональный сайт Александра Шангина">
-    <meta property="og:image:width" content="500">
-    <meta property="og:image:height" content="500">
+    <meta property="og:image:width" content="387">
+    <meta property="og:image:height" content="387">
     <meta property="og:url" content="{{ URL::to('/') }}">
     <meta property="og:image" content="{{ URL::to('/') }}/og/og_index.png">
 
