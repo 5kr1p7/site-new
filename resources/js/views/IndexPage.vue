62988d6f69b39c6e30fdbceea47f8af84c23b20f
Alexander Shangin <5kr1p7@gmail.com>
2018-11-30 12:02:39 +0300

    Set layout for index page and add image

--- resources/js/views/IndexPage.vue
+++ resources/js/views/IndexPage.vue
@@ -1,7 +1,14 @@
 <template>
-    <v-layout align-center justify-center column>
-        <p class="text-xs-center display-1">Сайт находится в разработке.</p>
-        <ContactButtons></ContactButtons>
+    <v-layout align-content-center row wrap>
+        <v-flex offset-md4 md4 offset-xs4 xs4>
+            <v-img src="/img/construct.svg" max-height="300" contain></v-img>
+        </v-flex>
+        <v-flex xs12>
+            <p class="text-xs-center display-1">Сайт находится в разработке.</p>
+        </v-flex>
+        <v-flex xs12 md4 offset-md4>
+            <ContactButtons></ContactButtons>
+        </v-flex>
     </v-layout>
 </template>
 
