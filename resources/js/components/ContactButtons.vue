aab408a344cafbbcc7f83ff114c6165e016fd975
Alexander Shangin <5kr1p7@gmail.com>
2018-11-30 09:50:03 +0300

    Contact button as component

---
+++ resources/js/components/ContactButtons.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-layout id="contactButtons" align-center justify-center row wrap>
+        <v-flex class="text-xs-center d-flex align-center" xs4 lg3 v-for="socButton in socButtons" :key="socButton.name">
+            <soc-contact-btn :link="socButton.link" :color="socButton.color" :name="socButton.name">{{ socButton.text }}</soc-contact-btn>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    import SocContactBtn from "./SocContactBtn";
+    export default {
+        name: "ContactButtons",
+
+        components: { SocContactBtn },
+        data() {
+            return {
+                socButtons: [
+                    { name: 'twitter', color: '#1da1f2', link: 'https://twitter.com/5kr1p7', text: 'Лента твиттера' },
+                    { name: 'vkontakte', color: '#45668e', link: 'https://vk.com/alexshangin', text: 'вКонтакте' },
+                    { name: 'linkedin', color: '#0077b5', link: 'https://www.linkedin.com/in/shangin/', text: 'Профиль на LinkedIn' },
+                    { name: 'instagram', color: '#833ab4', link: 'https://www.instagram.com/5kr1p7/', text: 'Профиль в Инстаграм' },
+                    { name: 'telegram', color: '#0088cc', link: 'https://t.me/skr1p7', text: 'Написать в телеграм' },
+                    { name: 'github', color: '#333', link: 'https://github.com/5kr1p7', text: 'Профиль на Гитхабе' },
+                    { name: 'skype', color: '#00aff0', link: 'skype:skr1p7?call' }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/SocContactBtn.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-tooltip bottom>
+        <v-btn fab dark large slot="activator" :color="color" :href="link" target="_blank">
+            <v-icon dark>socicon-{{ name }}</v-icon>
+        </v-btn>
+        <span>
+            {{ $slots.default[0].text.length ? $slots.default[0].text : name | capitalize }}
+        </span>
+    </v-tooltip>
+</template>
+
+<script>
+    export default {
+        name: "SocContactBtn",
+
+        props: [ 'color', 'link', 'name' ],
+
+        filters: {
+            capitalize: function (value) {
+                if (!value) return ''
+                value = value.toString()
+                return value.charAt(0).toUpperCase() + value.slice(1)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
