dd0bb649442c40323756eafa94b64222c68bf189
Alexander Shangin <5kr1p7@gmail.com>
2018-12-03 12:22:39 +0300

    Add Post resource

---
+++ database/migrations/2018_12_03_112252_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->smallInteger('user_id')->unsigned()->comment('Автор');
+            $table->string('title', 255)->comment('Название поста');
+            $table->string('slug')->comment('Адрес Slug');
+            $table->text('text')->comment('Текст поста');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
