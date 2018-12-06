abc5260756f1cf223d7adecaa2bf10cdd37b109a
Alexander Shangin <5kr1p7@gmail.com>
2018-12-06 14:01:19 +0300

    Yandex metrika

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -30,3 +30,21 @@
     <link rel="dns-prefetch" href="//unpkg.com">
     <link rel="dns-prefetch" href="//cdn.jsdelivr.net">
 
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")
+
+        ym(51448531, "init", {
+            id:51448531,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/51448531" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
