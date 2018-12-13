b43fcd7405dea9467143f22e8cb79fe63cc36bff
Alexander Shangin <5kr1p7@gmail.com>
2018-12-14 01:40:38 +0300

    Update

--- resources/js/views/PostPage.vue
+++ resources/js/views/PostPage.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <v-layout row wrap>
+        <v-layout row wrap >
             <v-flex xs12>
                 <h1 class="content-title display-1">{{ cur_post.title }}</h1>
             </v-flex>
